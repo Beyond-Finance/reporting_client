5ca014afe8b577739982d53f902965b0ba833d6e
Ashley <azagorski@beyondfinance.com>
2022-07-18 16:03:35 -0500

    reverse classes

--- lib/reporting_client/event.rb
+++ lib/reporting_client/event.rb
@@ -3,14 +3,35 @@
 require 'active_support'
 require 'rubygems'
 
-require_relative 'events'
-
 module ReportingClient
-  class Event < Events
+  class Event
+    attr_reader :heap_identity, :land
+
+    def initialize(heap_identity: nil, land: nil)
+      @heap_identity = heap_identity
+      @land = land
+    end
+
+    def instrument(success:, fail_reason: nil, meta: {})
+      data = { success: success }
+      data.merge!(fail_reason: fail_reason) if fail_reason.present?
+      data.merge!(meta) if meta.present?
+      send_event(data)
+    end
+
     private
 
     def instrumentable_name
       @instrumentable_name ||= ReportingClient.configuration.instrumentable_name
+    end
+
+    def send_event(data)
+      data.merge! Current.attributes if defined?(Current) && Current.attributes.present?
+
+      ActiveSupport::Notifications.instrument(instrumentable_name, data)
+
+      Heap.call(identity: heap_identity, event_name: instrumentable_name, properties: data) if heap_identity.present?
+      land.queue_event(instrumentable_name, data) if land.present?
     end
   end
 end
