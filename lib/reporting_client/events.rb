3515278d9f42d4a79eb1d4c1c4414ba8d04fc8cc
Jacob Daddario <jacob.d@hey.com>
2023-03-29 22:00:59 -0500

    Update lib/reporting_client/events.rb
    Co-authored-by: Harrison Strowd <harrison@haresoft.dev>

--- lib/reporting_client/events.rb
+++ lib/reporting_client/events.rb
@@ -27,7 +27,7 @@ module ReportingClient
         ActiveSupport::Notifications.subscribe(event_name) do |name, _start, _finish, _id, payload|
           payload.merge!(Current.attributes.compact) if defined?(Current) && Current.attributes.present?
 
-          name = "#{config.instrumentable_name}_" + name.to_s if config.prefix_new_relic_names
+          name = "#{config.instrumentable_name}_#{name.to_s}" if config.prefix_new_relic_names
           ::NewRelic::Agent.record_custom_event(name.to_s, payload)
         end
       end
