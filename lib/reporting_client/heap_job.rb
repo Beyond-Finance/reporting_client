88ee02eccac327a0d66080d1e12195af0f638192
Afra Ahmad <git@afra.io>
2024-01-15 22:28:14 -0500

    Rubocop RubyTargetVersion to 3.2 and spacing fix

--- lib/reporting_client/heap_job.rb
+++ lib/reporting_client/heap_job.rb
@@ -5,7 +5,7 @@ require_relative './heap'
 
 module ReportingClient
   class HeapJob < ActiveJob::Base
-    def perform(event_name, identity, properties, timestamp=Time.now.iso8601)
+    def perform(event_name, identity, properties, timestamp = Time.now.iso8601)
       Heap.new(event_name: event_name, identity: identity, properties: properties, timestamp: timestamp).track
     end
   end
